2635aecf732653a887df2f01184fcccfda905580
Alessandro Soldani <alessandro.soldani26@gmail.com>
2021-12-05 20:43:35 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -4,26 +4,25 @@ questions.forEach(function(question){
   const btn=question.querySelector("#btn");
   const answer=question.querySelector(".answer");
   const quest=question.querySelector(".question-text");
-  const icon=question.querySelector(".question-text img");
-  btn.addEventListener("click",function(){
-    closeAll();
-    if(icon.classList.contains("rot-arrow")){
-        closeAll();
-        console.log("ciao");
-    }
+  quest.addEventListener("click",function(){
+
+  if(btn.classList.contains('rot-arrow'))
+      closeAll();
+
     else{
-      console.log("A ciao");
+    closeAll();
     answer.classList.add("show-answer");
     quest.style.fontWeight="700";
-    icon.classList.add("rot-arrow");
+    btn.classList.add("rot-arrow");
     };
+    
   })
 });
 
 
 const allAnswers=document.querySelectorAll(".answer");
 const allQuest=document.querySelectorAll(".question-text");
-const allIcon=document.querySelectorAll(".question-text img");
+const allButton=document.querySelectorAll(".question-text button");
 function closeAll(quest,ico){
   allAnswers.forEach(function(item){
     item.classList.remove("show-answer");
@@ -31,7 +30,7 @@ function closeAll(quest,ico){
   allQuest.forEach(function(item){
     item.style.fontWeight="100";
   });
-  allIcon.forEach(function(item){
+  allButton.forEach(function(item){
     item.classList.remove("rot-arrow");
   });
 };
